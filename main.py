c34bce804245a4400a2fc84ebc35df1f275f2965
Lorenzo <lorenzo.millucci@outlook.com>
2013-11-28 21:39:10 +0100

    aggiunti gli slider per modificare le soglie di colore

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ def onTrackbarSlide(*args):
     pass
 
 
+
+
 def createSlider():
 	
 	cv2.namedWindow(settingWindow,1);
@@ -32,6 +34,7 @@ def createSlider():
 	cv2.createTrackbar("H-max",settingWindow, H_MAX, 256, onTrackbarSlide)
 	cv2.createTrackbar("S-max",settingWindow, S_MAX, 256,onTrackbarSlide)
 	cv2.createTrackbar("V-max",settingWindow, V_MAX, 256,onTrackbarSlide)
+	
 
 
 # ------- MAIN -------------
@@ -44,24 +47,29 @@ cv2.namedWindow(mainGui,1)
 capture = cv2.VideoCapture(0);
 width,height = capture.get(3),capture.get(4)
 
-
+createSlider()
 #loop principale del programma
 while True:
 	#definisco la variabile per i frame catturati
 	_,cameraFeed = capture.read()
 	cameraFeed = cv2.flip(cameraFeed,1)
+	
+	#variabile su cui salvo l'immagine HSV
+	hsvFrame = cv2.cvtColor(cameraFeed,cv2.COLOR_BGR2HSV)
 
 
-	createSlider()
+	
 
-	colorMin=np.array([H_MIN,S_MIN,V_MIN],np.uint8)
 	#filtro hsvFrame cercando solo un determinato range di colori
-	#cv2.InRange(hsvFrame, colorMin, colorMin,thresholded);
+	minColor=np.array((cv2.getTrackbarPos("H-min",settingWindow),cv2.getTrackbarPos("S-min",settingWindow),cv2.getTrackbarPos("V-min",settingWindow)))
+	maxColor=np.array((cv2.getTrackbarPos("H-max",settingWindow),cv2.getTrackbarPos("S-max",settingWindow),cv2.getTrackbarPos("V-max",settingWindow)))
+	thresholded=cv2.inRange(hsvFrame,minColor, maxColor);
 	
 	
 	#visualizzo le immagini 
 	cv2.imshow(mainGui,cameraFeed)
-	#cv2.ShowImage(hsvWindow, hsvFrame)
+	#cv2.imshow(hsvWindow, hsvFrame)
+	cv2.imshow(thresholdWindow,thresholded)
 	
 	if cv2.waitKey(33)==27:
 		break
